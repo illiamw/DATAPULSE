19c6b979a23d490fcf4e77bc2458bbcb2d45bf81
William Ferreira <33575357+illiamw@users.noreply.github.com>
2026-09-20 01:05:51 -0300

    Testando fluxo

--- src/app_mlops/main.py
+++ src/app_mlops/main.py
@@ -93,7 +93,7 @@ def root():
     """
     Health check endpoint.
     """
-    return {"status": "ok_v03"}
+    return {"status": "ok_v04"}
 
 
 # ============================================================
